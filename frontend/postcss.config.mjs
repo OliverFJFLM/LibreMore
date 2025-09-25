b8edcb51d96d71300e7d1f823d36086bdc12e1d6
OliverFJFLM <ayyt7cwa@gmail.com>
2025-09-26 00:09:42 +0900

    Update postcss.config.mjs

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,6 +1,6 @@
-import tailwind from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwind, autoprefixer]
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 };
